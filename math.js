21396947350db81979ba37457d6d46acdc719d61
Emiliano Rotta <emilianorottaf@gmail.com>
2024-11-19 20:23:36 -0300

    ejercicios completos

--- math.js
+++ math.js
@@ -5,20 +5,30 @@
 
 //npm install chai mocha --save-dev
 
-// export function suma (a, b){
-//     if(typeof a !== "number" || typeof b !== "number"){
-//         throw new Error('Ambos argumentos deben ser numeros')
-//     }
-//     return  a + b;
-// }
+export function suma (a, b){
+    if(typeof a !== "number" || typeof b !== "number"){
+        throw new Error('Ambos argumentos deben ser numeros')
+    }
+    return  a + b;
+}
 
 // Consigna
 // Crea una función llamada resta(a, b) que devuelva la resta de dos números.
 // Escribe un test que verifique que la función funciona correctamente para los casos:
 // resta(10, 5) debe devolver 5.
 // resta(0, 3) debe devolver -3.
+// export function resta(a, b) {
+//     return a - b
+// }
 
 // Modifica la función resta(a, b) para que valide que ambos argumentos sean números.
+export function resta(a, b) {
+    if(typeof a !== "number" || typeof b !== "number"){
+        throw new Error('Ambos argumentos deben ser numeros')
+    }
+    return a - b
+}
+
 
 // Escribe un test que verifique:
 // La resta funciona correctamente cuando los argumentos son números.
--- test/example.test.js
+++ test/example.test.js
@@ -1,5 +1,5 @@
 import { expect } from 'chai';
-import { suma } from '../math.js';
+import { resta, suma } from '../math.js';
 
 describe('Operaciones matemáticas suma', () => {
     it('Debería sumar dos números correctamente', () => {
@@ -16,5 +16,34 @@ describe('Operaciones matemáticas suma', () => {
         expect(()=> suma(2, undefined)).to.throw('Ambos argumentos deben ser numeros');
     });
 
+
+})
+
+// Escribe un test que verifique que la función funciona correctamente para los casos:
+// resta(10, 5) debe devolver 5.
+// resta(0, 3) debe devolver -3.
+
+describe('Operaciones matemáticas resta', () => {
+    it('Debería restar dos números correctamente', () => {
+        const resultado = resta(10, 5);
+        expect(resultado).to.equal(5);
+    });
+    it('Debería poder restar dos números y quedar en negativo', () => {
+        const resultado = resta(0, 3);
+        expect(resultado).to.equal(-3);
+    });
+    it('Debería arrojar error si no es un número', () => {
+        expect(()=> resta(2,"3")).to.throw('Ambos argumentos deben ser numeros');
+        expect(()=> resta("2","3")).to.throw('Ambos argumentos deben ser numeros');
+        expect(()=> resta("2",3)).to.throw('Ambos argumentos deben ser numeros');
+    });
+    it('Debería arrojar error si es null', () => {
+        expect(()=> resta(2,null)).to.throw('Ambos argumentos deben ser numeros');
+
+    });
+    it('Debería arrojar error si es undefined', () => {
+        expect(()=> resta(2,undefined)).to.throw('Ambos argumentos deben ser numeros');
+
+    });
 
 })
